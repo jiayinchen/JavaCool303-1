25b32e5fd26dcbc8996a82808240f23b10f2fe5a
Yu Qi Liu <yu.qi.liu236@gmail.com>
2016-11-26 22:43:31 -0500

    Basic structure

---
+++ JavaCool303/src/JavaCool303/Cool303Box.java
@@ -0,0 +1,10 @@
+package JavaCool303;
+
+/**
+ * 
+ * @author JiaYin Chen #260690708, Yu Qi Liu #260687081
+ *
+ */
+public class Cool303Box {
+
+}
---
+++ JavaCool303/src/JavaCool303/Cool303Button.java
@@ -0,0 +1,11 @@
+
+package JavaCool303;
+
+/**
+ * 
+ * @author JiaYin Chen #260690708, Yu Qi Liu #260687081
+ *
+ */
+public class Cool303Button {
+
+}
