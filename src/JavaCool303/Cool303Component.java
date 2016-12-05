d7a07a0208c50aaf744f34dc14c5ca9bd29d26c7
jiayinchen <jiayin.chen15@gmail.com>
2016-12-05 00:04:04 -0500

    added code (not all useful)

--- src/JavaCool303/Cool303Component.java
+++ src/JavaCool303/Cool303Component.java
@@ -1,7 +1,14 @@
 package JavaCool303;
 
 import javax.swing.JComponent;
+import javax.swing.*;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory.*;
+import javax.swing.border.*;
 /**
  * 
  * @author JiaYin Chen #260690708, Yu Qi Liu #260687081
@@ -9,11 +16,59 @@ import javax.swing.JComponent;
  */
 public class Cool303Component extends JComponent {
 
+	ArrayList<Cool303Component> compList=new ArrayList<Cool303Component>();
 	/**
-	 * Constructs new Cool303Component
+	 * 
 	 */
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * Constructor
+	 * */
 	public Cool303Component(){
 		super();
+	
+	}
+
+	/*
+	 * @param Graphics g 
+	 * @param int[] color*/
+	public void paint(Graphics g, int[] color){
+		super.paint(getGraphics());
+		setBackground(new Color(color[0], color[1], color[2]));
+	}
+	public void paintBorder(Graphics g, int[] color){
+		super.paintBorder(getGraphics());
+		setBackground(new Color(color[0], color[1], color[2]));
+	}
+	public void setBorder(Border bName){
+		super.setBorder(bName);;	
+	}
+	/*
+	 * @param Graphics g 
+	 * @param int[] color*/
+	public void paintComponent(Graphics g, int[] color){
+		super.paintComponent(getGraphics());
+		setBackground(new Color(color[0], color[1], color[2]));
+	}
+	/*
+	 * @param Cool303Component comp
+	 * */
+	public void add(Cool303Component comp){
+		this.compList.add(comp);
+	}
+	/*
+	 * @param Cool303Component comp
+	 * @returns an array of all the Cool303Components
+	 * */
+	public Cool303Component[] getComponents(){
+		Cool303Component[] ans=new Cool303Component[this.compList.size()];
+		int i=0;
+		for(Cool303Component c:this.compList){
+			ans[i]=c;
+			i++;
+		}
+		return ans; 
 	}
 	
 }
